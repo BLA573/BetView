eb8ede04d3ff9b869547dc1ed8277395a87ac963
BLA573 <hailemichaelsolomon176@gmail.com>
2026-05-08 11:18:59 +0300

    modified:package-lock.json,package.json,src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { ThemeProvider } from "@/contexts/ThemeContext";
 import AdminRouteGuard from "@/components/auth/AdminRouteGuard";
 import AgencyRouteGuard from "@/components/auth/AgencyRouteGuard";
 import AuthRequiredGuard from "@/components/auth/AuthRequiredGuard";
+import { Analytics } from "@vercel/analytics/next"
 
 const Index = lazy(() => import("./pages/Index"));
 const Browse = lazy(() => import("./pages/Browse"));
@@ -51,6 +52,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
+    <Analytics />
     <ThemeProvider>
       <TooltipProvider>
         <Toaster />
